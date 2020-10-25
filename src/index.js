048bb7f8e3470f13e9c16e7545097cd0f78c5fbc
Ilya Kaltovich <ilyakaltovich7@gmail.com>
2020-10-25 21:21:03 +0300

    feat: complite brackets-task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    for (let i = 0; i < bracketsConfig.length; i++) {
+        let bracketsCollection = bracketsConfig[i].join('');
+        if (str.includes(bracketsCollection)) {
+          i -= 1;
+          str = str.replace(bracketsCollection, '');
+        }
+    }
+    if (!str)
+        return true
+    else
+        return false
 }
