6eac379e77108f020df7873d25357b01b6b78c87
Ilya Kaltovich <ilyakaltovich7@gmail.com>
2020-10-25 22:24:53 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ module.exports = function check(str, bracketsConfig) {
     for (let i = 0; i < bracketsConfig.length; i++) {
         let bracketsCollection = bracketsConfig[i].join('');
         if (str.includes(bracketsCollection)) {
-          i -= 1;
+          i = - 1;
           str = str.replace(bracketsCollection, '');
         }
     }
